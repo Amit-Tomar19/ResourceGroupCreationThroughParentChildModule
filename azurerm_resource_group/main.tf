8c6d9737c3fc3c49ce55d23bd5016e9a753cc6a6
Amit Tomar <amittomar19@gmail.com>
2025-07-22 10:42:46 +0530

    Resource Group Creation Through Parent Child Module

---
+++ azurerm_resource_group/main.tf
@@ -0,0 +1,6 @@
+resource azurerm_resource_group "example" {
+#   name     = "example-resource-group"
+#   location = "West Europe"
+name = var.name
+location = var.location
+}
---
+++ infra/main.tf
@@ -0,0 +1,7 @@
+module "azurerm_resource_group" {
+  source = "../azurerm_resource_group"
+
+  name     = "amit-resource-group"
+  location = "West Europe"
+
+}
